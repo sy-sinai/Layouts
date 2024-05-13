02b1c0cdb9c1ca328f608b717198966ddae4dc00
laboratorio <laboratorio@UPEAULA-31710>
2024-05-13 08:44:27 -0500

    Agregar archivos de proyecto.

---
+++ proyecto2/Pages/GridLayout.xaml.cs
@@ -0,0 +1,9 @@
+namespace proyecto2.Pages;
+
+public partial class GridLayout : ContentView
+{
+	public GridLayout()
+	{
+		InitializeComponent();
+	}
+}
---
+++ proyecto2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace proyecto2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
