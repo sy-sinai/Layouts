fff926196a50e147a5a96c60db05cb70d31bd75d
sy-sinai <122645408+sy-sinai@users.noreply.github.com>
2024-05-15 23:55:07 -0500

    Update FlexLayout.xaml.cs

--- proyecto2/Pages/FlexLayout.xaml.cs
+++ proyecto2/Pages/FlexLayout.xaml.cs
@@ -2,8 +2,14 @@ namespace proyecto2.Pages;
 
 public partial class FlexLayout : ContentPage
 {
-	public FlexLayout()
-	{
-		InitializeComponent();
-	}
+    public FlexLayout()
+    {
+        InitializeComponent();
+    }
+
+    public async void Click (object sender, TappedEventArgs e)
+    {
+        await Navigation.PushAsync(new AbsoluteLayout());
+    }
+
 }
